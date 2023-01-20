077a389cd9536b2388c3d745e51d5386c5b4e3c4
UnknownCommiter <93703619+UnknownCommiter@users.noreply.github.com>
2023-01-20 14:25:24 +0200

    Add files via upload

---
+++ upload/catalog/language/ru/extension/module/omniva_m.php
@@ -0,0 +1,13 @@
+<?php
+
+// JS Map
+$_['omniva_m_modal_header'] = 'Пакомат Omniva';
+$_['omniva_m_shipping_method_terminal'] = 'Omniva: Пакомат';
+$_['omniva_m_search_bar_title'] = 'Адреса пакоматов';
+$_['omniva_m_select_terminal'] = 'Выбрать пакомат';
+$_['omniva_m_search_bar_placeholder'] = 'Введите почт.индекс/адрес';
+$_['omniva_m_show_on_map'] = 'Показать на карте';
+$_['omniva_m_show_more'] = 'Показать ещё';
+$_['omniva_m_place_not_found'] = 'Место не найдено';
+$_['omniva_m_search_back_to_list'] = 'Назад к списку';
+$_['omniva_m_terminal_not_selected'] = 'Пожалуйсто выберите пакомат';
---
+++ upload/catalog/language/ru/extension/shipping/omniva_m.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title'] = 'Omniva';
+$_['text_prefix'] = 'Omniva: ';
+$_['text_courier'] = 'Курьер';
+$_['text_description'] = 'Стоимость доставки за единицу товара';
