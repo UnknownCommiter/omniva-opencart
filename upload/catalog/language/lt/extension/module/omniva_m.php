31b78c2a24907176786fa7fd8f917f8bff15c962
UnknownCommiter <93703619+UnknownCommiter@users.noreply.github.com>
2023-01-20 14:24:24 +0200

    Lithuanian translation

---
+++ upload/catalog/language/lt/extension/module/omniva_m.php
@@ -0,0 +1,13 @@
+<?php
+
+// JS Map
+$_['omniva_m_modal_header'] = 'Omniva paštomatas';
+$_['omniva_m_shipping_method_terminal'] = 'Omniva: Paštomatas';
+$_['omniva_m_search_bar_title'] = 'Paštomatų adresai';
+$_['omniva_m_select_terminal'] = 'Pasirinkti paštomata';
+$_['omniva_m_search_bar_placeholder'] = 'Įveskite pašto kodą / adresą';
+$_['omniva_m_show_on_map'] = 'Parodyti žemėlapyje';
+$_['omniva_m_show_more'] = 'Parodyti daugiau';
+$_['omniva_m_place_not_found'] = 'Vieta nerasta';
+$_['omniva_m_search_back_to_list'] = 'Atgal į sąrašą';
+$_['omniva_m_terminal_not_selected'] = 'Prašome pasirinkti paštomata';
---
+++ upload/catalog/language/lt/extension/shipping/omniva_m.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title'] = 'Omniva';
+$_['text_prefix'] = 'Omniva: ';
+$_['text_courier'] = 'Kurjeris';
+$_['text_description'] = 'Vienos prekės pristatymo tarifas';
