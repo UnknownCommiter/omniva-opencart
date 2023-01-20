bb78b950770c249afdcf00a8b6c6534fce1ddb45
UnknownCommiter <93703619+UnknownCommiter@users.noreply.github.com>
2023-01-20 14:23:38 +0200

    Estonian translation

---
+++ upload/catalog/language/et/extension/module/omniva_m.php
@@ -0,0 +1,13 @@
+<?php
+
+// JS Map
+$_['omniva_m_modal_header'] = 'Omniva pakiautomaadid';
+$_['omniva_m_shipping_method_terminal'] = 'Omniva: Pakiautomaadid';
+$_['omniva_m_search_bar_title'] = 'Pakiautomaadide aadressid';
+$_['omniva_m_select_terminal'] = 'Valige pakiautomaadi';
+$_['omniva_m_search_bar_placeholder'] = 'Sisestage sihtnumber/aadress';
+$_['omniva_m_show_on_map'] = 'Näita kaardil';
+$_['omniva_m_show_more'] = 'Näita rohkem';
+$_['omniva_m_place_not_found'] = 'Asukohta ei leitud';
+$_['omniva_m_search_back_to_list'] = 'Tagasi nimekirja';
+$_['omniva_m_terminal_not_selected'] = 'Palun valige pakiautomaadi';
---
+++ upload/catalog/language/et/extension/shipping/omniva_m.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title'] = 'Omniva';
+$_['text_prefix'] = 'Omniva: ';
+$_['text_courier'] = 'Kuller';
+$_['text_description'] = 'Ühe eseme kohta saatmishind';
