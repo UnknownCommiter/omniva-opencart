56fb04240ba3eb1c497fc4c9a85c18ecf579d711
UnknownCommiter <93703619+UnknownCommiter@users.noreply.github.com>
2023-01-20 14:24:57 +0200

    Latvian translation

---
+++ upload/catalog/language/lv/extension/module/omniva_m.php
@@ -0,0 +1,13 @@
+<?php
+
+// JS Map
+$_['omniva_m_modal_header'] = 'Omniva pakomāts';
+$_['omniva_m_shipping_method_terminal'] = 'Omniva: pakomāts';
+$_['omniva_m_search_bar_title'] = 'Pakomātu adreses';
+$_['omniva_m_select_terminal'] = 'Izvēlēties pakomātu';
+$_['omniva_m_search_bar_placeholder'] = 'Ievadiet pasta indeksu/adresi';
+$_['omniva_m_show_on_map'] = 'Rādīt kartē';
+$_['omniva_m_show_more'] = 'Rādīt vēl';
+$_['omniva_m_place_not_found'] = 'Vieta nav atrasta';
+$_['omniva_m_search_back_to_list'] = 'Atpakaļ uz sarakstu';
+$_['omniva_m_terminal_not_selected'] = 'Lūdzu, izvēlieties pakomātu';
---
+++ upload/catalog/language/lv/extension/shipping/omniva_m.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title'] = 'Omniva';
+$_['text_prefix'] = 'Omniva: ';
+$_['text_courier'] = 'Kurjers';
+$_['text_description'] = 'Piegādes izmaksas par preces vienību';
